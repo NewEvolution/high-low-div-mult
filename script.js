70f59d2acca8910b7facc784b3507ab87783e405
Ryan Tanay <ryantanay@newevolution.org>
2015-07-22 09:03:18 -0500

    Added bolding to the party, made the input button clear the page text if no number is entered.

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ function testerFunc() {
   var inputBox = document.getElementById("num_input");
   var userInput = inputBox.value;
   if(userInput === "") {
+    writeFunc("");
     alert("Please enter a number.");
   } else if (userInput > 10000) {
     mathFunc(userInput, diviFunc);
   } else if (userInput < 10000) {
     mathFunc(userInput, multiFunc);
   } else {
-    writeFunc("<span class='party'>You found the edge case!<br>WOOO PARTY!</span>");
+    writeFunc("<span class='party'>You found the edge case!<br><strong>WOOO PARTY!</strong></span>");
   }
   inputBox.value = "";
 }
